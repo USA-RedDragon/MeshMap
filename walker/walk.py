d336c588cd8041c9b68606af3d22a9b7eac4bd61
Jacob McSwain <jacob@mcswain.dev>
2023-08-26 06:00:22 -0500

    Use a Python walker

---
+++ walker/walk.py
@@ -0,0 +1,109 @@
+import datetime
+import json
+from json.decoder import JSONDecodeError
+import os
+
+import aiohttp
+import asyncio
+from aiohttp_retry import RetryClient, RandomRetry
+from aiohttp.client_exceptions import ClientConnectionError, ClientConnectorError, ServerDisconnectedError, ClientResponseError, ClientPayloadError
+from aiohttp.http_exceptions import BadHttpMessage
+
+_ignored_exceptions = (ConnectionRefusedError, ClientConnectorError, ClientConnectionError, ClientPayloadError, ClientResponseError, BadHttpMessage, JSONDecodeError, asyncio.TimeoutError, ServerDisconnectedError, UnicodeDecodeError)
+
+class AsyncWalk:
+    def __init__(self, starting_node, max_concurrency=100, attempts=5, timeout_seconds=60):
+        self._client = RetryClient(
+            client_session=aiohttp.ClientSession(
+                connector=aiohttp.TCPConnector(limit=max_concurrency),
+                timeout=aiohttp.ClientTimeout(total=timeout_seconds),
+                headers={"User-Agent": "KI5VMF-MeshMap Walker (https://meshmap.aredn.mcswain.dev/)"},
+            ),
+            retry_options=RandomRetry(
+                attempts=attempts,
+                min_timeout=timeout_seconds,
+                max_timeout=timeout_seconds+10,
+                exceptions=_ignored_exceptions,
+                statuses=[i for i in range(205,300)] + [i for i in range(300,600)],
+            ),
+        )
+        self._starting_node = starting_node
+        self._tasks = []
+        self._data = []
+
+    async def _fetch(self, url):
+        async with self._client.get(url) as response:
+            return await response.json(content_type=None)
+
+    async def run(self):
+        total_tasks = 1
+        completed = 1
+        all_seen = set()
+        try:
+            resp = await self._fetch(f"http://{self._starting_node}.local.mesh:8080/cgi-bin/sysinfo.json?hosts=1&link_info=1&lqm=1")
+        except _ignored_exceptions as _:
+            pass
+        for host in resp["hosts"]:
+            url = f"http://{host['name']}.local.mesh:8080/cgi-bin/sysinfo.json?hosts=1&link_info=1&lqm=1"
+            all_seen.add(host['name'])
+            self._tasks.append(asyncio.ensure_future(self._fetch(url)))
+            total_tasks += 1
+
+        all_seen.add(self._starting_node)
+
+        while len(self._tasks) > 0:
+            new_tasks = []
+            done, tasks = await asyncio.wait(self._tasks, return_when=asyncio.FIRST_COMPLETED)
+            for f in done:
+                completed += 1
+                print(f"\33[2K\rCompleted {completed} tasks out of {total_tasks}, {total_tasks-completed} left. Found {len(all_seen)} hosts, {len(self._data)} AREDN nodes.", end="")
+                try:
+                    response = await f
+                    if response and "node" in response and response["node"]:
+                        for host in response["hosts"]:
+                            if host['name'] not in all_seen:
+                                all_seen.add(host['name'])
+                                url = f"http://{host['name']}.local.mesh:8080/cgi-bin/sysinfo.json?hosts=1&link_info=1&lqm=1"
+                                new_tasks.append(asyncio.ensure_future(self._fetch(url)))
+                                total_tasks += 1
+
+                        if "lat" in response and "lon" in response and response["lat"] and response["lon"]:
+                            if "link_info" not in response:
+                                response["link_info"] = {}
+                            self._data.append({
+                                "data": {
+                                    "node": response["node"],
+                                    "lastseen": response["lastseen"] if "lastseen" in response else None,
+                                    "lat": response["lat"],
+                                    "lon": response["lon"],
+                                    "meshrf": response["meshrf"],
+                                    "chanbw": response["chanbw"] if "chanbw" in response else None,
+                                    "node_details": response["node_details"],
+                                    "interfaces": response["interfaces"],
+                                    "link_info": [response["link_info"][key] for key in response["link_info"]],
+                                    "lqm": response["lqm"] if "lqm" in response else None,
+                                },
+                            })
+                except _ignored_exceptions as _:
+                    continue
+            self._tasks = list(tasks) + new_tasks
+
+        print()
+        return self._data
+
+    async def stop(self):
+        await self._client.close()
+
+async def main():
+    walk = AsyncWalk(starting_node="KI5VMF-CLOUD-TUNNEL-supernode")
+    node_info = await walk.run()
+    await walk.stop()
+    print(f"Found {len(node_info)} nodes.")
+    with open("/usr/share/nginx/html/data/out.json.new", "w") as f:
+        json.dump({
+            "nodeInfo": node_info,
+            "date": datetime.datetime.utcnow().isoformat(),
+        }, f)
+    os.rename("/usr/share/nginx/html/data/out.json.new", "/usr/share/nginx/html/data/out.json")
+
+asyncio.run(main())
