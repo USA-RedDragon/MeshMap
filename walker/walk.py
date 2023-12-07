1a6a6aa442d3c83122a96a718c6b93201387dfe3
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-12-06 19:50:21 -0600

    walker: reduce concurrency

--- walker/walk.py
+++ walker/walk.py
@@ -13,7 +13,7 @@ from aiohttp.http_exceptions import BadHttpMessage
 _ignored_exceptions = (ConnectionRefusedError, ClientConnectorError, ClientConnectionError, ClientPayloadError, ClientResponseError, BadHttpMessage, JSONDecodeError, asyncio.TimeoutError, ServerDisconnectedError, UnicodeDecodeError)
 
 class AsyncWalk:
-    def __init__(self, starting_node, max_concurrency=2000, attempts=10, timeout_seconds=120):
+    def __init__(self, starting_node, max_concurrency=200, attempts=10, timeout_seconds=120):
         self._client = RetryClient(
             client_session=aiohttp.ClientSession(
                 connector=aiohttp.TCPConnector(limit=max_concurrency, force_close=True),
