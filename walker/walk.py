62c09c1c8a19982b6ba6e4225b183e544e25191c
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-02-14 19:58:26 -0600

    walker: update supernode connections

--- walker/walk.py
+++ walker/walk.py
@@ -75,8 +75,8 @@ class AsyncWalk:
 
                             if "node_details" in response and "mesh_supernode" in response["node_details"] and response["node_details"]["mesh_supernode"]:
                                 for key in response["link_info"]:
-                                    if response["link_info"][key]["linkType"] == "TUN":
-                                        response["link_info"][key]["linkType"] = "STUN"
+                                    if response["link_info"][key]["linkType"] == "TUN" || response["link_info"][key]["linkType"] == "WIREGUARD":
+                                        response["link_info"][key]["linkType"] = "SUPER"
 
                             self._data.append({
                                 "data": {
